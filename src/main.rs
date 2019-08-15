426d076d77a1727593f068b678ad3987bbf663bd
Christian Goldapp <c.goldapp@tu-bs.de>
2019-08-15 23:10:23 +0200

    Add in Config importing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::BufReader;
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let config = load_config();
+    match config {
+        Ok(c) => {
+            println!("{:?}", c);
+        }
+        Err(e) => {
+            eprintln!("Error reading config file! {}", e);
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    cookie: String,
+    server: String,
+}
+
+fn load_config() -> Result<Config, Box<Error>> {
+    let file = File::open("DynoTagManagerConfig.json")?;
+    let reader = BufReader::new(file);
+    let c: Config = serde_json::from_reader(reader)?;
+    Ok(c)
 }
