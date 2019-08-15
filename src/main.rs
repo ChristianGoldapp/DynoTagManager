aee0fa3789fcf753029f2ab490df0d911e023cda
Christian Goldapp <c.goldapp@tu-bs.de>
2019-08-16 00:50:57 +0200

    Implement list_tags

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::fs::File;
 use std::io::BufReader;
 use std::error::Error;
 use reqwest::{Client, header::{HeaderValue, HeaderMap, COOKIE}, Response};
+use serde_json::Value;
 
-fn main() -> Result<(), Box<Error>>{
+fn main() -> Result<(), Box<Error>> {
     let config = load_config()?;
-    println!("{:?}", config);
-
+    println!("{:?}", config.list_tags());
     Ok(())
 }
 
@@ -24,7 +24,7 @@ struct Tag {
 }
 
 impl DynoInstance {
-    fn create_tag(&self, tag: Tag) -> Result<Response, Box<Error>>{
+    fn create_tag(&self, tag: Tag) -> Result<Response, Box<Error>> {
         let url = format!("https://dyno.gg/api/server/{}/tags/create", self.server);
         let headers = get_headers(&self)?;
         let http = Client::builder().default_headers(headers).build()?;
@@ -32,6 +32,25 @@ impl DynoInstance {
             serde_json::to_string(&tag)?
         ).send()?;
         Ok(response)
+    }
+
+    fn list_tags(&self) -> Result<Vec<Tag>, Box<Error>> {
+        let url = format!("https://dyno.gg/api/server/{}/tags/list", self.server);
+        let headers = get_headers(&self)?;
+        let http = Client::builder().default_headers(headers).build()?;
+        let mut response = http.get(url.as_str()).send()?;
+        let json: Value = response.json()?;
+        let tag_json: &Value = &json["tags"];
+        let vec : Vec<Tag> = match &tag_json {
+            &Value::Array(v) => {
+                v.into_iter().map(|json| Tag { tag: json["tag"].to_string(), content: json["content"].to_string() }).collect()
+            }
+            _ => {
+                eprintln!("Could not parse JSON returned from server!");
+                vec![]
+            }
+        };
+        Ok(vec)
     }
 }
 
