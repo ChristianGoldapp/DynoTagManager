a495167657250f87955cc4925e5752ca5a2b7f9c
Christian Goldapp <c.goldapp@tu-bs.de>
2019-08-18 22:05:07 +0200

    Implement commands

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,29 @@ use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::BufReader;
 use std::error::Error;
-use reqwest::{Client, header::{HeaderValue, HeaderMap, COOKIE}, Response};
+use reqwest::{Client, header::{HeaderValue, HeaderMap, COOKIE, CONTENT_TYPE}, Response};
 use serde_json::Value;
 use serde::export::fmt::Debug;
 use std::fmt::{Formatter, Display};
 use std::fmt;
+use std::env::args;
 
 fn main() -> Result<(), Box<Error>> {
     let config = load_config()?;
-    println!("{:#?}", config.list_tags()?);
+    let args: Vec<_> = args().collect();
+    println!("{:?}", args);
+    if args[1] == "list" {
+        println!("{:#?}", config.list_tags()?);
+    } else if args[1] == "create" {
+        let tag = Tag {
+            tag: args[2].to_string(),
+            content: args[3].to_string(),
+        };
+        match config.create_tag(tag) {
+            Ok(_) => {println!("Tag successfully created!\n{}", tag)}
+            Err(e) => Err(e)
+        }
+    }
     Ok(())
 }
 
@@ -35,11 +49,17 @@ impl Debug for Tag {
 impl DynoInstance {
     fn create_tag(&self, tag: Tag) -> Result<Response, Box<Error>> {
         let url = format!("https://dyno.gg/api/server/{}/tags/create", self.server);
-        let headers = get_headers(&self)?;
-        let http = Client::builder().default_headers(headers).build()?;
-        let response = http.post(url.as_str()).body(
-            serde_json::to_string(&tag)?
-        ).send()?;
+        let mut headers = get_headers(&self)?;
+        headers.insert(CONTENT_TYPE, HeaderValue::from_str("application/json")?);
+        println!("{:?}", headers);
+        let http = Client::builder().build()?;
+        let body = serde_json::to_string(&tag)?;
+        println!("{}", body);
+        let request = http.post(url.as_str())
+            .headers(headers)
+            .body(body);
+        println!("{:?}", request);
+        let response = request.send()?;
         Ok(response)
     }
 
@@ -50,7 +70,7 @@ impl DynoInstance {
         let mut response = http.get(url.as_str()).send()?;
         let json: Value = response.json()?;
         let tag_json: &Value = &json["tags"];
-        let vec : Vec<Tag> = match &tag_json {
+        let vec: Vec<Tag> = match &tag_json {
             &Value::Array(v) => {
                 v.into_iter().map(|json| Tag { tag: json["tag"].to_string(), content: json["content"].to_string() }).collect()
             }
